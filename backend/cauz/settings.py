72b89beae1bbaffe1fb418da75623b75f1b0b163
Luis Mario Suárez <lmcervantessuarez@gmail.com>
2025-02-05 13:34:17 -0600

    Agregar configuración de Docker Compose para el servicio de backend en Django y base de datos MySQL, actualizar dependencias y ajustar configuración de conexión a la base de datos

--- backend/cauz/settings.py
+++ backend/cauz/settings.py
@@ -75,10 +75,14 @@ WSGI_APPLICATION = 'cauz.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'cauz',
+		'USER': 'root',
+		'PASSWORD': 'edqnLMCSf31.',
+		'HOST':'database',
+		'PORT':'3306',
+	}
 }
 
 
