2a99b0f69cdce5d45131ce8addc777514e96fe44
Luis Mario Cervantes Suarez <lmcervantessuarez@gmail.com>
2025-02-06 15:30:07 -0600

    Agregar soporte para drf-spectacular, incluyendo configuración de documentación y nuevas rutas para la API

--- backend/cauz/settings.py
+++ backend/cauz/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'postulantes.apps.PostulantesConfig',
     'rest_framework.authtoken',
     'solicitud_eventos.apps.SolicitudEventosConfig',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -131,8 +132,18 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+    # Authentication
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    # Documentation
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'SaaS Backend API - Cauz',
+    'DESCRIPTION': 'Documentación de la API de Cauz',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
 }
--- backend/cauz/urls.py
+++ backend/cauz/urls.py
@@ -17,11 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('postulantes/api/', include('postulantes.urls')),
     path('solicitud_eventos/api/', include('solicitud_eventos.urls')),
-    path('api-token-auth/', views.obtain_auth_token)
+    path('api-token-auth/', views.obtain_auth_token),
+    # Documentacion
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Documentacion UI:
+    path('api/schema/ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
