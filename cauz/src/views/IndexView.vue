bd10338f3d4ce2fa3fb773d0e20aacfd1aad4507
Luis Mario Suárez <lmcervantessuarez@gmail.com>
2025-02-11 12:03:51 -0600

    Agregar configuración inicial y archivos para la aplicación Cauz con Vue y Vite

---
+++ cauz/src/views/IndexView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import NavBar from '@/components/NavBar.vue'
+</script>
+<template>
+  <div class="hero bg-base-200 min-h-screen">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-6xl font-bold">Cauz</h1>
+        <h1 class="text-5xl font-bold">Foro & Libreria</h1>
+
+        <p class="py-6">
+          Aquí, cada presentación es una experiencia única, cada historia encuentra su voz y cada
+          expresión artística conecta con quienes buscan inspiración.
+        </p>
+        <div class="flex justify-center space-x-4">
+          <button class="btn btn-primary">¡Ver eventos!</button>
+          <RouterLink to="/solicitud" class="btn btn-neutral">Quiero presentarme</RouterLink>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ cauz/src/views/SolicitudView.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <h1>Formulario de solicitud</h1>
+</template>
